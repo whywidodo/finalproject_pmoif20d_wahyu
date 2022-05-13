27c063eb149436663cf3037e977a5df42402e16f
fitrohiza <fitrohiza@gmail.com>
2022-05-13 14:49:50 +0700

    tambah file dari fitroh

---
+++ lib/FitrohIzatulMalkiyah.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FitrohIzatulMalkiyah extends StatefulWidget {
+  const FitrohIzatulMalkiyah({Key? key}) : super(key: key);
+
+  @override
+  _FitrohIzatulMalkiyahState createState() => _FitrohIzatulMalkiyahState();
+}
+
+class _FitrohIzatulMalkiyahState extends State<FitrohIzatulMalkiyah> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
