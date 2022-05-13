582dd85e34ddc7d246570d87ffd0a75eb89e274c
devanilarassati <devanilarassati@gmail.com>
2022-05-13 23:02:32 +0700

    tambahan devani laras sati

---
+++ lib/DevaniLarasSati.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DevaniLarasSati extends StatefulWidget {
+  const DevaniLarasSati({Key? key}) : super(key: key);
+
+  @override
+  State<DevaniLarasSati> createState() => _DevaniLarasSatiState();
+}
+
+class _DevaniLarasSatiState extends State<DevaniLarasSati> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
