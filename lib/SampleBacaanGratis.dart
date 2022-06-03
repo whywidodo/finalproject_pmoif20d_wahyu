af5d84e96b614bb04e29236269d03d9ead9bc38e
nilanazilatulmazaya <nilanazilatul@gmail.com>
2022-06-03 09:04:41 -0700

    create Sample Bacaan Gratis

---
+++ lib/SampleBacaanGratis.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SampleBacaanGratis extends StatefulWidget {
+  const SampleBacaanGratis({Key? key}) : super(key: key);
+
+  @override
+  _SampleBacaanGratisState createState() => _SampleBacaanGratisState();
+}
+
+class _SampleBacaanGratisState extends State<SampleBacaanGratis> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.chevron_left),
+          onPressed: (){
+            print('clicked once');
+
+          },
+        ),
+        centerTitle: true,
+        title: const Text(
+          'Malin Kundang',
+          style: TextStyle(fontFamily: 'PoppinsMedium'),
+        ),
+        backgroundColor: const Color(0xFF6A2B84),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.share),
+              onPressed: () {
+            },
+          )
+        ],
+
+      ),
+    );
+  }
+}
